c448f47648c6222739107bac98d67a7eab01a74f
Kyllian Maignan <shokkzu@gmail.com>
2023-11-17 16:32:34 +0100

    Modify : Added sendEmail to addItem Method

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -2,11 +2,15 @@ import java.util.ArrayList;
 
 public class ToDoList{
     private ArrayList<Item> itemList;
+    private EmailSenderService emailSenderService;
 
     public void addItem(Item item){
         try{
             if (itemList.size() <= 10){
                 itemList.add(item);
+                if (itemList.size() == 8){
+                    this.emailSenderService.sendEmail();
+                }
             }else{
                 throw new ItemListFullException();
             }
