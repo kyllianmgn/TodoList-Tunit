ac2a0e383fde0f454d8dca1a91b092976721765f
Arvik <94380788+arviik@users.noreply.github.com>
2023-11-17 18:47:57 +0100

    Added parameters in ToDoListTest

--- test/ToDoListTest.java
+++ test/ToDoListTest.java
@@ -1,6 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.mockito.Mock;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -8,6 +13,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class ToDoListTest {
+
+    static class UserArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return Stream.of(
+                    Arguments.of(new User("ipsum.primis@yahoo.couk", "ipsum", "ipsum", "ipsumipsumipsum", 20)),
+                    Arguments.of(new User("at.sem@google.edu", "at", "at", "atatat", 20)),
+                    Arguments.of(new User("bibendum@aol.couk", "bibendum", "bibendum", "bibendumbibendumbibendum", 20)),
+                    Arguments.of(new User("tristique.pharetra.quisque@yahoo.com", "tristique", "tristique", "tristiquetristiquetristique", 20)),
+                    Arguments.of(new User("sit.amet@hotmail.net", "sit", "sit", "sitsitsit", 20))
+            );
+        }
+    }
 
     @Mock
     private User user;
@@ -31,7 +49,7 @@ class ToDoListTest {
     }
 
     @Test
-    void createToDoList(){
+    void createToDoList() {
 
     }
 
