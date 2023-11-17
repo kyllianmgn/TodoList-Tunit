eacad95f5ac210018cb4aed7a84b5689b894fdd5
Kyllian Maignan <shokkzu@gmail.com>
2023-11-17 15:06:53 +0100

    Add : User Class

---
+++ src/User.java
@@ -0,0 +1,55 @@
+public class User {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private int age;
+
+    public User(String email, String firstName, String lastName, String password, int age) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
